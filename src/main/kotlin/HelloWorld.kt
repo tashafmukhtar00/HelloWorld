5826f2461e9f54de8be18e834d55804651c962cb
tashafmukhtar <tashafmukhtar@gmail.com>
2022-05-19 18:24:18 +0400

    Hello World program in kotlin

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,7 @@
+// print hello world
+
+
+fun main() {    //This is the main function, which is mandatory in every Kotlin application. The Kotlin compiler starts executing the code from the main function.
+    println("Hello World")   // Simply prints the text on console
+    print("I am Learning Kotlin")  // Prints the text on console and add blank line at the end
+}
